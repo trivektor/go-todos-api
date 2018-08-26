12c26063698774452eaef37f19be5da77e2b71a3
Tri Vuong <trivektor@gmail.com>
2018-08-25 20:54:07 -0700

    update todos index endpoint

--- controllers/todo.go
+++ controllers/todo.go
@@ -2,16 +2,34 @@ package controllers
 
 import (
   "net/http"
-  "fmt"
+  "encoding/json"
   "github.com/julienschmidt/httprouter"
 
   "todos/database"
+  "todos/models"
 )
 
 // _ is the blank identifier
 // https://stackoverflow.com/questions/37079820/meaning-of-underscore-in-a-go-function-parameter
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  fmt.Fprintf(w, "Todos API")
+  rows, _ := database.DBConn.Query("SELECT * FROM todos")
+  defer rows.Close()
+  todo := models.Todo{}
+  todos := []models.Todo{}
+
+  for rows.Next() {
+    var id int
+    var description string
+    rows.Scan(&id, &description)
+    todo.Id = id
+    todo.Description = description
+    todos = append(todos, todo)
+  }
+
+  response, _ := json.Marshal(todos)
+  w.Header().Set("Content-Type", "application/json")
+  w.WriteHeader(200)
+  w.Write(response)
 }
 
 func Create(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
---
+++ models/todo.go
@@ -0,0 +1,6 @@
+package models
+
+type Todo struct {
+  Id int
+  Description string
+}
